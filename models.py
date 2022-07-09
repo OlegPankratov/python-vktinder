767ecb88df609dd19a725e1aaca47dcf9e1bc40f
Viktor Pankratov <v.pankratov@itech-group.ru>
2022-07-09 17:33:15 +0400

    Приложение - вк бот, аналог тиндера. Для работы бота нужны ключи - группы и персональной - вставить в файле services/VkService.py. Для поиска нужно написать боту "Ищи"

---
+++ models.py
@@ -0,0 +1,50 @@
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    age = Column(String(30))
+    sex = Column(String(30))
+    name = Column(String)
+    city = Column(String)
+    vk_id = Column(Integer, unique=True)
+    photos = relationship(
+        "Photo", back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, name={self.name!r}, city={self.city!r})"
+
+
+class Photo(Base):
+    __tablename__ = "photo"
+    id = Column(Integer, primary_key=True)
+    vk_id = Column(Integer, unique=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    user = relationship("User", back_populates="photos")
+
+    def __repr__(self):
+        return f"Photo(id={self.id!r}, path={self.vk_id!r})"
+
+
+class SearchHistory(Base):
+    __tablename__ = "search_history"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    pair_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Address(user_id={self.user_id!r}, pair_id={self.pair_id!r})"
+
+
+engine = create_engine("sqlite:///db.sqlite3", echo=True, future=True)
+Base.metadata.create_all(engine)
---
+++ services/VkService.py
@@ -0,0 +1,43 @@
+from random import randrange
+
+import vk_api
+
+group_token = '#'  # Токен группы
+personal_token = '#'  # Персональный токен
+
+
+class VkService:
+    def __init__(self):
+        self.group_vk = vk_api.VkApi(token=group_token)
+        self.personal_vk = vk_api.VkApi(token=personal_token)
+
+    def get_group_vk(self):
+        return self.group_vk
+
+    def write_msg(self, user_id, message, attachment=''):
+        self.group_vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7),
+                                               'attachment': attachment})
+
+    def get_user_info(self, user_id):
+        return self.group_vk.method('users.get',
+                                    {'user_ids': user_id, 'fields': 'sex,bdate,city,status', 'name_case': 'Nom'})
+
+    def get_all_user_photos(self, user_id):
+        all_user_photos = self.personal_vk.method('photos.getAll', {'owner_id': user_id})
+        return all_user_photos
+
+    def get_user_photo_likes(self, user_id):
+        user_photos = self.get_all_user_photos(user_id)
+        photo_like_counts = {}
+        for user_photo in user_photos['items']:
+            item_id = user_photo['id']
+            likes = self.personal_vk.method('likes.getList', {'type': 'photo', 'owner_id': user_id, 'item_id': item_id})
+            photo_like_counts[item_id] = likes['count']
+        return photo_like_counts
+
+    def get_users_by_filter(self, filter):
+        request_body = filter
+        request_body['fields'] = 'sex,bdate,city,status,nickname,photo_50'
+        request_body['sort'] = 0
+        return self.personal_vk.method("users.search", request_body)
+
